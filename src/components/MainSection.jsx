e389f87199818818d922a93e9c626b674153686d
Kaviselvan-SJ <sjkaviselvan07@gmail.com>
2025-07-09 17:49:11 +0530

    UI updated

--- src/components/MainSection.jsx
+++ src/components/MainSection.jsx
@@ -1,5 +1,17 @@
 export const MainSection = () => {
   return (
-    <div>Main Content</div>
+    <div className="relative min-h-screen flex flex-col items-center justify-center px-4 gap-3">
+      <h1>
+        Code space
+      </h1>
+      <h3>
+        code -{'>'} summit -{'>'} repeat
+      </h3>
+      <p>
+        Want to practice Data structures and Algorithm ,
+        Want to improve problem solving skills, this platform is for you
+      </p>
+  
+    </div>
   )
 }
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,13 +1,14 @@
 import { Navbar } from "../components/NavBar";
 import { ThemeToggle } from "../components/ThemeToggle";
 import { Footer } from "../components/Footer";
+import { MainSection } from "../components/MainSection";
 
 export const Home = () => {
   return(
     <div>
       <ThemeToggle />
       <Navbar />
-      <h1>Home Page</h1>
+      <MainSection />
       <Footer />
     </div>
   );
